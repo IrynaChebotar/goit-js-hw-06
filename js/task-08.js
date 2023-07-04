a73f351d5badc30dc07ca34a8f09f83431dfc93a
IrynaChebotar <iryna.chebotar2@gmail.com>
2023-07-04 19:44:45 +0200

    9

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,14 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onSend);
+
+function onSend(event) {
+event.preventDefault();
+const { elements:{email,password}} = event.currentTarget;
+if (email.value === "" || password.value === "" ){
+    alert('Please fill in all the fields');
+} 
+    console.log(`email: ${email.value}, password:${password.value}`);
+event.currentTarget.reset();
+}
 
--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const bodyColor = document.querySelector('body');
+const nameColor = document.querySelector('.color');
+const colorBtn = document.querySelector('.change-color');
+
+
+colorBtn.addEventListener('click', changeColor);
+
+function changeColor() {
+const newColor = getRandomHexColor();
+bodyColor.style.backgroundColor = newColor;
+nameColor.textContent = newColor;
+}
