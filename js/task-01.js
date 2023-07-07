c197ba949845e9228f5dcd05c9c2435c3ab8b98e
IrynaChebotar <iryna.chebotar2@gmail.com>
2023-07-07 15:27:40 +0200

    1-7

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,13 @@
-const categoryItems = document.querySelectorAll("#categories > li.item");
+const categoriesElement = document.getElementById("categories");
+const categoryItems = categoriesElement.children;
 const itemsCount = categoryItems.length;
-console.log(`Number of categories: ${ itemsCount } `);
-categoryItems.forEach(category => {
+
+console.log(`Number of categories: ${itemsCount}`);
+
+Array.from(categoryItems).forEach(category => {
   const categoryName = category.querySelector("h2").textContent;
-  const categoryElements = category.querySelectorAll("li").length;
+  const categoryElements = category.children.length - 1; // Віднімаємо 1, оскільки потрібно виключити заголовок <h2>
+
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements}`);
 });
--- js/task-07.js
+++ js/task-07.js
@@ -1,9 +1,12 @@
+
 const inputEl = document.querySelector('#font-size-control');
 const textSize = document.querySelector('#text');
 
+window.addEventListener('DOMContentLoaded', onChange);
 
 inputEl.addEventListener('input', onChange);
 
 function onChange() {
-textSize.style.fontSize = inputEl.value + 'px';
-};
+  textSize.style.fontSize = `${inputEl.value}px`;
+}
+
